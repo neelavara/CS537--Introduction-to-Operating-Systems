6fbfdf988e8e13ec9822bcf699eac7a93ea8e8b9
Arpith Neelavara <36017003+neelavara@users.noreply.github.com>
2018-01-31 17:37:13 -0600

    Create README_Shuffle.c

---
+++ README_Shuffle.c
@@ -0,0 +1,14 @@
+You will write a shuffling program. It should alternate printing lines starting at the beginning of the file moving forward and lines at the end of the file moving backwards. So, your output should alternate with lines from the beginning of the file and lines from the end. With this input:
+
+     first
+     second
+     third
+     fourth
+     fifth
+the program should produce this output:
+
+     first
+     fifth
+     second
+     fourth
+     third
